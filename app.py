f6993ff80b5bd4f3a3f1e603d45382bcc99aa30b
Adrian Looney <adrian.looney@gmail.com>
2021-09-11 19:41:55 +0100

    al_created company appilcation form

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route('/createProfile')
+def create_profile():
+    return render_template("createProfile.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
