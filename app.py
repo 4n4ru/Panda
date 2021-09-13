8454549d6f5a36af2404a06b09c2b1ede519308b
Brianconn71 <Brian.connolly71@gmail.com>
2021-09-13 13:57:52 +0000

    add: set debug to false

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def company_profile(company_name):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
